9e6d204e4194889f5f51ac88da6e6c73fa565671
Osiris Mariano <oslimacontato@gmail.com>
2023-12-05 19:48:02 -0300

    Layout Pagina de login(new/entar)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,31 +8,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    flash[:success] = 'Usuário cadastrado com sucesso'
-    redirect_to root_url
+      flash[:success] = 'Usuário cadastrado com sucesso'
+      redirect_to root_url
     else
       render 'new'
-    end
-
-    def edit
-
-
-    end
-
-    def update
-      if current_user.update(user_params)
-        flash[:success] = "Dados Atualizados"
-        redirect_to contact_url
-      else
-        render 'edit'
-      end
-
     end
   end
 
   private
-  def user_params
-    params.require(:user).permit(:email, :name, :password_confirmation)
+    def user_params
+      params.require(:user).permit(:email, :name, :password, :passwod_confirmation)
 
-  end
+    end
+
 end
